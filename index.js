4b3e90c8572de60b6be0c7e56eebd2af83d49aec
iamjoedeveloper <joesuarez.developer@gmail.com>
2022-08-22 21:35:42 -0400

    Resolvers

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import { gql, ApolloServer } from 'apollo-server'
+// import { ApolloServer } from 'apollo-server-express'
+
 const persons = [
 	{
 		name: 'Joe',
@@ -13,4 +16,42 @@ const persons = [
 		city: 'España',
 		id: '30123123',
 	},
+	{
+		name: 'Arisela',
+		phone: '04127859655',
+		street: 'Happy Street',
+		city: 'Miami',
+		id: '13726951',
+	},
 ]
+
+const typeDefinitions = gql`
+	type Person {
+		name: String!
+		phone: String
+		street: String!
+		city: String!
+		id: ID!
+	}
+
+	type Query {
+		personCount: Int!
+		allPersons: [Person]!
+	}
+`
+
+const resolvers = {
+	Query: {
+		personCount: () => persons.length,
+		allPersons: () => persons,
+	},
+}
+
+const server = new ApolloServer({
+	typeDefs: typeDefinitions,
+	resolvers,
+})
+
+server.listen().then(({ url }) => {
+	console.log(`Server ready at ${url}`)
+})
