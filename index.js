f6bb53ec1307fde48c96e011c097cf972d4f3c33
iamjoedeveloper <joesuarez.developer@gmail.com>
2022-08-22 20:29:19 -0400

    Graphql Server base files

---
+++ index.js
@@ -0,0 +1,16 @@
+const persons = [
+	{
+		name: 'Joe',
+		phone: '8292821107',
+		street: 'Frontend Av.',
+		city: 'Toronto - Canada',
+		id: '23626777',
+	},
+	{
+		name: 'Gabriella',
+		phone: '04125559655',
+		street: 'Europa Street',
+		city: 'España',
+		id: '30123123',
+	},
+]
